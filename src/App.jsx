521a52355d341eda3250430aad9c2c0e13a0632e
Danny Le <le.dannyj@gmail.com>
2021-05-26 13:56:24 -0400

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <h1>Color Selector!</h1>
+      </>
+    )
   }
 }
