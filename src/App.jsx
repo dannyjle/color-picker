bebe8c27e7acd946053a13e2c8ebc316fe4da006
Danny Le <le.dannyj@gmail.com>
2021-05-26 14:06:39 -0400

    Linked State to Hue, Saturation, and Lightness

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,34 @@ export class App extends Component {
   render() {
     return (
       <>
-        <h1>Color Selector!</h1>
+        <body>
+          <h1>Color Selector!</h1>
+          <div>Pick your color:</div>
+          <p>Hue:{this.state.hue}</p>
+          <input
+            type="range"
+            value={this.state.hue}
+            max={0}
+            min={0}
+            onChange={event => this.setState({ hue: event.target.value })}
+          />
+          <p>Saturation:{this.state.saturation}</p>
+          <input
+            type="range"
+            value={this.state.saturation}
+            max={0}
+            min={0}
+            onChange={event => this.setState({ hue: event.target.value })}
+          />
+          <p>Lightness:{this.state.lightness}</p>
+          <input
+            type="range"
+            value={this.state.lightness}
+            max={0}
+            min={0}
+            onChange={event => this.setState({ hue: event.target.value })}
+          />
+        </body>
       </>
     )
   }
