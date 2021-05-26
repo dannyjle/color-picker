d3740c817c49f1fd9772801764dbc2b333cc57da
Danny Le <le.dannyj@gmail.com>
2021-05-26 15:53:52 -0400

    Added Color Box and Random Color Refresh

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,56 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   state = {
-    hue: 0,
-    saturation: 0,
-    lightness: 0,
+    hue: Math.floor(Math.random() * (359 - 1) + 1),
+    saturation: Math.floor(Math.random() * (100 - 1) + 1),
+    lightness: Math.floor(Math.random() * (100 - 1) + 1),
+    alpha: Math.floor(Math.random() * (100 - 1) + 1),
   }
 
   render() {
+    const newBackgroundColor = `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`
+
+    const newStyle = { backgroundColor: newBackgroundColor }
+
     return (
       <>
-        <body>
+        <main>
           <h1>Color Selector!</h1>
           <h4>Pick your color:</h4>
-          <div className={'color'}></div>
-          <p>Hue: {this.state.hue}</p>
-          <input
-            type="range"
-            value={this.state.hue}
-            max={360}
-            min={0}
-            onChange={event => this.setState({ hue: event.target.value })}
-          />
-          <p>Saturation: {this.state.saturation}</p>
-          <input
-            type="range"
-            value={this.state.saturation}
-            max={100}
-            min={0}
-            onChange={event =>
-              this.setState({ saturation: event.target.value })
-            }
-          />
-          <p>Lightness: {this.state.lightness}</p>
-          <input
-            type="range"
-            value={this.state.lightness}
-            max={100}
-            min={0}
-            onChange={event => this.setState({ lightness: event.target.value })}
-          />
-        </body>
+          <div className="colorBox" style={newStyle}>
+            {`hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`}
+          </div>
+          <article>
+            <p>Hue: {this.state.hue}</p>
+            <input
+              type="range"
+              value={this.state.hue}
+              max={360}
+              min={0}
+              onChange={event => this.setState({ hue: event.target.value })}
+            />
+            <p>Saturation: {this.state.saturation}%</p>
+            <input
+              type="range"
+              value={this.state.saturation}
+              max={100}
+              min={0}
+              onChange={event =>
+                this.setState({ saturation: event.target.value })
+              }
+            />
+            <p>Lightness: {this.state.lightness}%</p>
+            <input
+              type="range"
+              value={this.state.lightness}
+              max={100}
+              min={0}
+              onChange={event =>
+                this.setState({ lightness: event.target.value })
+              }
+            />
+          </article>
+        </main>
       </>
     )
   }
