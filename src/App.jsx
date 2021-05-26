a0c7884588e8f30ce248dbda42ecc915b3a4a48d
Danny Le <le.dannyj@gmail.com>
2021-05-26 14:17:43 -0400

    Fixed Range Bug

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,33 @@ export class App extends Component {
       <>
         <body>
           <h1>Color Selector!</h1>
-          <div>Pick your color:</div>
-          <p>Hue:{this.state.hue}</p>
+          <h4>Pick your color:</h4>
+          <div className={'color'}></div>
+          <p>Hue: {this.state.hue}</p>
           <input
             type="range"
             value={this.state.hue}
-            max={0}
+            max={360}
             min={0}
             onChange={event => this.setState({ hue: event.target.value })}
           />
-          <p>Saturation:{this.state.saturation}</p>
+          <p>Saturation: {this.state.saturation}</p>
           <input
             type="range"
             value={this.state.saturation}
-            max={0}
+            max={100}
             min={0}
-            onChange={event => this.setState({ hue: event.target.value })}
+            onChange={event =>
+              this.setState({ saturation: event.target.value })
+            }
           />
-          <p>Lightness:{this.state.lightness}</p>
+          <p>Lightness: {this.state.lightness}</p>
           <input
             type="range"
             value={this.state.lightness}
-            max={0}
+            max={100}
             min={0}
-            onChange={event => this.setState({ hue: event.target.value })}
+            onChange={event => this.setState({ lightness: event.target.value })}
           />
         </body>
       </>
